eed228d5657865f267bff723375d0863a9650dfb
Eduardo Moreira <edumartins1w@gmail.com>
2020-02-12 15:50:10 -0300

    template para app console .netcore 3.0 c/ appsettings e serilog

---
+++ NetCore3Console/Opcoes.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetCore3Console
+{
+    public class Opcoes
+    {
+        public string Teste { get; set; }
+    }
+}
---
+++ NetCore3Console/Program.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Events;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Threading;
+
+namespace NetCore3Console
+{
+    class Program
+    {
+        private static Opcoes _opcoes;
+
+        static void Main(string[] args)
+        {
+            ConfigurarLogs();
+            SetGlobalCultureInfo();
+            _opcoes = LerOpcoes();
+
+            
+        }
+
+        static Opcoes LerOpcoes()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            IConfiguration configuration = builder.Build();
+
+            var opcoes = new Opcoes()
+            {
+                Teste = configuration.GetValue<string>("Teste")
+            };
+
+            return opcoes;
+        }
+
+        static void ConfigurarLogs()
+        {
+
+#if DEBUG
+            Serilog.Debugging.SelfLog.Enable(msg => Debug.WriteLine(msg));
+#endif
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+#if DEBUG
+                .WriteTo.ColoredConsole(
+                    restrictedToMinimumLevel: LogEventLevel.Information,
+                    outputTemplate: "{NewLine}{Timestamp:HH:mm:ss} [{Level}] {Message}{NewLine}{Exception}"
+                )
+#endif
+
+
+                .WriteTo.File(
+                    path: "logs\\serilog.txt",
+                    rollingInterval: RollingInterval.Day,
+                    retainedFileCountLimit: 31
+                )
+                .CreateLogger();
+        }
+
+        static void SetGlobalCultureInfo()
+        {
+            var cultureInfo = new CultureInfo("pt-BR");
+            Thread.CurrentThread.CurrentCulture = cultureInfo;
+        }
+    }
+}
